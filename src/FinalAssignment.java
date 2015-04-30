0b5a403544ce162729770478f73197f20a166952
ambarket <ambarket@gmail.com>
2015-04-29 22:27:14 -0400

    fdag

--- src/FinalAssignment.java
+++ src/FinalAssignment.java
@@ -21,9 +21,9 @@ public class FinalAssignment {
 		StopWatch watch = new StopWatch().start();
 		
 		//Preprocessor.createTabDelimitedFileFromRawDataset(BASE_PATH, "Books.txt", "Books-tab-delim-80charReview.txt", 80);
-		Preprocessor.createFileWithOnlyKMostReviewedProducts(NUMBER_OF_PRODUCTS, BASE_PATH, "Books-tab-delim.txt", MOST_REVIEWED);
-		Preprocessor.createFileWithOnlyKMostActiveUsers(NUMBER_OF_USERS, BASE_PATH, MOST_REVIEWED, MOST_REVIEWED_AND_MOST_ACTIVE);
-		Preprocessor.integerizeUserAndProductIds(BASE_PATH, MOST_REVIEWED_AND_MOST_ACTIVE, MOST_REVIEWED_AND_MOST_ACTIVE_WITH_INTEGER_IDS);
+		//Preprocessor.createFileWithOnlyKMostReviewedProducts(NUMBER_OF_PRODUCTS, BASE_PATH, "Books-tab-delim.txt", MOST_REVIEWED);
+		//Preprocessor.createFileWithOnlyKMostActiveUsers(NUMBER_OF_USERS, BASE_PATH, MOST_REVIEWED, MOST_REVIEWED_AND_MOST_ACTIVE);
+		//Preprocessor.integerizeUserAndProductIds(BASE_PATH, MOST_REVIEWED_AND_MOST_ACTIVE, MOST_REVIEWED_AND_MOST_ACTIVE_WITH_INTEGER_IDS);
 		
 		ArrayList<IntegerRating> allRatings = new ArrayList<IntegerRating>();
 		allRatings.addAll(Preprocessor.readIntegerRatingsFromFile(BASE_PATH, MOST_REVIEWED_AND_MOST_ACTIVE_WITH_INTEGER_IDS));
@@ -35,22 +35,10 @@ public class FinalAssignment {
 		//System.out.println("UVDecomp rmsle: " + rmsle);
 		
 		NaiveBayes naiveBayes = new NaiveBayes();
-		//double naiveBayesRMSLE = naiveBayes.crossValidateAndReturnRMSLE(allRatings, 10);
-		
 		double naiveBayesAccuracy = naiveBayes.crossValidateAndReturnAccuracy(allRatings, 10);
 		System.out.println("NaiveBayes accuracy: " + naiveBayesAccuracy);
 		
 		System.out.println("Ran in " + watch.getElapsedSeconds() + " seconds.");
-		
-		// required for UVDecomp
-		//ArrayList<Rating> allRatings = readFileIntoRatingsList();
-		
-		// Map userIds to their ratings.
-		//HashMap<String, Rating> trainingRatingsByUserId = new HashMap<String, Rating>();
-		// Map productIds to their ratings.
-		//HashMap<String, Rating> trainingRatingsByProductId = new HashMap<String, Rating>();
-
-
 	}
 	
 
--- src/NaiveBayes.java
+++ src/NaiveBayes.java
@@ -6,67 +6,13 @@ import java.util.Random;
 import java.util.Set;
 
 class NaiveBayes {
-	// userId --> productId --> ratingValue
+	
 	HashMap<Integer, HashSet<IntegerRating>> productToRatingsMap;
 	HashMap<Integer, HashSet<IntegerRating>> userToRatingsMap;
-	//IntegerRating[][][] trainingRatings;
+	
 	int[][][] trainingRatings;
 	
 	double globalAverage;
-	//HashMap <String, HashMap<String, Integer>> trainingRatings;
-	
-	/*
-	public double crossValidateAndReturnRMSLE(ArrayList<Rating> allRatings, int numberOfFolds) {
-		Collections.shuffle(allRatings);
-		
-		int ratingsPerFold = allRatings.size() / numberOfFolds;
-		
-		ArrayList<HashSet<Rating>> folds = new ArrayList<HashSet<Rating>>();
-		int i;
-		HashSet<Rating> tmp = new HashSet<Rating>();
-		for (i = 0; i < numberOfFolds-1; i++) {
-			tmp = new HashSet<Rating>();
-			tmp.addAll(allRatings.subList(i * ratingsPerFold, (i+1) * ratingsPerFold));
-			folds.add(tmp);
-			//System.out.println("created fold " + i + " " + folds.get(i).size());
-		}
-		tmp = new HashSet<Rating>();
-		tmp.addAll(allRatings.subList(i * ratingsPerFold, allRatings.size()));
-		folds.add(tmp);
-		//System.out.println("created fold " + i + " " + folds.get(i).size());
-		
-		double sumOfRMSLE = 0.0;
-		
-		StopWatch watch = new StopWatch();
-		i = 0;
-		for (HashSet<Rating> leaveOutForPrediction : folds) {		
-			watch.start();
-			
-			trainingRatings = new HashMap <String, HashMap<String, Integer>>();
-			for (Rating rating : allRatings) {
-				if (!leaveOutForPrediction.contains(rating)) {
-					if (!trainingRatings.containsKey(rating.userId)) {
-						trainingRatings.put(rating.userId, new HashMap<String, Integer>());
-					}
-					trainingRatings.get(rating.userId).put(rating.productId, rating.ratingValue);
-				}
-			}
-			
-			double rmsle = 0;
-			for (Rating target : leaveOutForPrediction) {
-				rmsle += Math.log(target.ratingValue) - Math.log(predict(target.userId, target.productId));
-			}
-			rmsle /= leaveOutForPrediction.size();
-			rmsle = Math.sqrt(rmsle);
-			sumOfRMSLE += rmsle;
-			
-			System.out.println("Finished fold " + i + " in " + watch.getElapsedSeconds() + " seconds.");
-			
-		}
-		
-		return sumOfRMSLE / folds.size();
-	}
-	*/
 	
 	public double crossValidateAndReturnAccuracy(ArrayList<IntegerRating> allRatings, int numberOfFolds) {
 		Collections.shuffle(allRatings);
@@ -94,76 +40,80 @@ class NaiveBayes {
 		for (HashSet<IntegerRating> leaveOutForPrediction : folds) {			
 			watch.start();
 			
-			productToRatingsMap = new HashMap<Integer, HashSet<IntegerRating>>();
-			userToRatingsMap = new HashMap<Integer, HashSet<IntegerRating>>();
-			//trainingRatings = new IntegerRating[FinalAssignment.NUMBER_OF_PRODUCTS][6][];//new HashMap <String, HashMap<String, Integer>>();
-			
-			// values are userIds that rated the product with value
-			trainingRatings = new int[FinalAssignment.NUMBER_OF_PRODUCTS][6][];//new HashMap <String, HashMap<String, Integer>>();
-			
-			// Map products and users to ratings of/by them, and find global average rating.
-			int numberOfRatings = 0;
-			for (IntegerRating rating : allRatings) {
-				if (!leaveOutForPrediction.contains(rating)) {
-					if (!productToRatingsMap.containsKey(rating.productId)) {
-						productToRatingsMap.put(rating.productId, new HashSet<IntegerRating>());
-					}
-					productToRatingsMap.get(rating.productId).add(rating);
-					
-					if (!userToRatingsMap.containsKey(rating.userId)) {
-						userToRatingsMap.put(rating.userId, new HashSet<IntegerRating>());
-					}
-					userToRatingsMap.get(rating.userId).add(rating);
-					
-					globalAverage += rating.ratingValue;
-					numberOfRatings++;
-				}
-			}
-			globalAverage /= numberOfRatings;
-			
-			// Pre sort product ratings into buckets by rating value to speed things up later
-			for (int productId : productToRatingsMap.keySet()) {
-				int[] counts = {productToRatingsMap.get(productId).size(),0,0,0,0,0}; // [0] will store all userIds that rated the product.
-				
-				for (IntegerRating rating : productToRatingsMap.get(productId)) {
-					counts[rating.ratingValue]++;
-				}
-				// Create the buckets of the right size
-				for (int num = 0; num < 6; num++) {
-					//trainingRatings[productId][num] = new IntegerRating[counts[num]];
-					trainingRatings[productId][num] = new int[counts[num]];
-				}
-				// Fill up the buckets
-				for (IntegerRating rating : productToRatingsMap.get(productId)) {
-					assert(counts[rating.ratingValue] > 0);
-					counts[rating.ratingValue]--;
-					trainingRatings[productId][rating.ratingValue][counts[rating.ratingValue]] = rating.userId;
-					
-					// Always add to the 0th bucket
-					assert(counts[0] > 0);
-					counts[0]--;
-					trainingRatings[productId][0][counts[0]] = rating.userId;
-				}
-			}
+			buildModel(allRatings, leaveOutForPrediction);
 			
 			// Make prediction for all the left out ratings. 
 			int correct = 0;
 			for (IntegerRating target : leaveOutForPrediction) {
 				watch.start();
 				int prediction = predict(target.userId, target.productId);
-				System.out.println("Actual: " + target.ratingValue + " Prediction: " + prediction );
+				//System.out.println("Actual: " + target.ratingValue + " Prediction: " + prediction );
 				if (target.ratingValue == prediction) {
 					correct++;
 				}
 				//System.out.println("Finished prediction " + i + " in " + watch.getElapsedSeconds() + " seconds.");
 			}
-			System.out.println("Correct: " + correct + " Predicted: " + leaveOutForPrediction.size() );
+			//System.out.println("Correct: " + correct + " Predicted: " + leaveOutForPrediction.size() );
 			sumOfAccuracy += correct / (double)leaveOutForPrediction.size();
 			
 			System.out.println("Finished fold " + i + " in " + watch.getElapsedSeconds() + " seconds.");
 		}
 		
 		return sumOfAccuracy / folds.size();
+	}
+	
+	public void buildModel(ArrayList<IntegerRating> allRatings, HashSet<IntegerRating> leaveOutForPrediction) {
+		productToRatingsMap = new HashMap<Integer, HashSet<IntegerRating>>();
+		userToRatingsMap = new HashMap<Integer, HashSet<IntegerRating>>();
+		//trainingRatings = new IntegerRating[FinalAssignment.NUMBER_OF_PRODUCTS][6][];//new HashMap <String, HashMap<String, Integer>>();
+		
+		// values are userIds that rated the product with value
+		trainingRatings = new int[FinalAssignment.NUMBER_OF_PRODUCTS][6][];//new HashMap <String, HashMap<String, Integer>>();
+		
+		// Map products and users to ratings of/by them, and find global average rating.
+		int numberOfRatings = 0;
+		for (IntegerRating rating : allRatings) {
+			if (!leaveOutForPrediction.contains(rating)) {
+				if (!productToRatingsMap.containsKey(rating.productId)) {
+					productToRatingsMap.put(rating.productId, new HashSet<IntegerRating>());
+				}
+				productToRatingsMap.get(rating.productId).add(rating);
+				
+				if (!userToRatingsMap.containsKey(rating.userId)) {
+					userToRatingsMap.put(rating.userId, new HashSet<IntegerRating>());
+				}
+				userToRatingsMap.get(rating.userId).add(rating);
+				
+				globalAverage += rating.ratingValue;
+				numberOfRatings++;
+			}
+		}
+		globalAverage /= numberOfRatings;
+		
+		// Pre sort product ratings into buckets by rating value to speed things up later
+		for (int productId : productToRatingsMap.keySet()) {
+			int[] counts = {productToRatingsMap.get(productId).size(),0,0,0,0,0}; // [0] will store all userIds that rated the product.
+			
+			for (IntegerRating rating : productToRatingsMap.get(productId)) {
+				counts[rating.ratingValue]++;
+			}
+			// Create the buckets of the right size
+			for (int num = 0; num < 6; num++) {
+				//trainingRatings[productId][num] = new IntegerRating[counts[num]];
+				trainingRatings[productId][num] = new int[counts[num]];
+			}
+			// Fill up the buckets
+			for (IntegerRating rating : productToRatingsMap.get(productId)) {
+				assert(counts[rating.ratingValue] > 0);
+				counts[rating.ratingValue]--;
+				trainingRatings[productId][rating.ratingValue][counts[rating.ratingValue]] = rating.userId;
+				
+				// Always add to the 0th bucket
+				assert(counts[0] > 0);
+				counts[0]--;
+				trainingRatings[productId][0][counts[0]] = rating.userId;
+			}
+		}
 	}
 	
 	
@@ -234,7 +184,7 @@ class NaiveBayes {
 		double givenCount = product2Users.length + 5;
 		double conditionalCount = 1;
 		
-		// TODO: Make structure for this if too slow and if so no need to store userIds anymore, 
+		// TODO: Make structure for this if too slow and if so no need to store userIds anymore in trainingRatings, 
 		// just need counts of number of users that rated each product with	each value
 		for (int p2UserId : product2Users) {
 			for (int p1UserId : product1Users) {
@@ -245,7 +195,7 @@ class NaiveBayes {
 			}
 		}
 		
-		System.out.println("Finished getCondProb in " + watch.getElapsedSeconds() + " seconds." + conditionalCount + "/" + givenCount);
+		//System.out.println("Finished getCondProb in " + watch.getElapsedSeconds() + " seconds." + conditionalCount + "/" + givenCount);
 		return conditionalCount / givenCount;
 	}
 }
